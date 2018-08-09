6c80e45db9fddded74a47d00232a48d4b720b62f
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-09 02:27:15 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
 
 
   def show
-    @song = Song.find)by(id: params[:id])
+    @song = Song.find_by(id: params[:id])
     if @song.artist.nil?
       redirect_to songs_path(@song), alert: "Song not found"
     end
